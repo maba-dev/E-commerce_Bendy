3de73352c1b5d042ee2e46548ee0532d4044435a
maba-dev <cherif.ba@hotmail.fr>
2022-07-03 15:50:53 +0000

    Récuperer les informations du formulaire, Afficher les utilisateurs connectés et créer la vue de connexion

--- shop/urls.py
+++ shop/urls.py
@@ -19,9 +19,13 @@ from django.contrib import admin
 from django.urls import path
 from store.views import index, product_detail
 from shop import settings
+from accounts.views import signup, logout_user, login_user
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
+    path('login/', login_user, name="login"),
+    path('logout/', logout_user, name="logout"),
+    path('signup/', signup, name="signup"),
     path('product/<str:slug>/', product_detail, name="product"),
     
 
--- store/views.py
+++ store/views.py
@@ -1,5 +1,3 @@
-
-import re
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
