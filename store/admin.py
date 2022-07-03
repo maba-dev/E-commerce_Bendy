3a829400b221bffa2fd887dbd8068cce656b2ccb
maba-dev <cherif.ba@hotmail.fr>
2022-07-03 18:26:18 +0000

    Planifier les models, creer le modèle Orders et le modèle Cart

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from  store.models import Product, Category
+from  store.models import Product, Category, Order, Cart
 # Register your models here.
 
 
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(Cart)
 
--- store/models.py
+++ store/models.py
@@ -1,13 +1,15 @@
 from audioop import reverse
+from pyexpat import model
+from statistics import mode
 from django.db import models
+from django.forms import modelformset_factory
+
+from shop.settings import AUTH_USER_MODEL
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
-  
-    @staticmethod
-    def get_all_categories():
-        return Category.objects.all()
-  
+
+
     def __str__(self):
         return self.name
 
@@ -27,3 +29,20 @@ class Product(models.Model):
     def get_absolute_url(self):
         return reverse("product", kwargs={"slug": self.slug})
 
+class Order(models.Model):
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.product.name} ({self.quantity})"
+
+class Cart(models.Model):
+    user = models.OneToOneField(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    orders = models.ManyToManyField(Order)
+    ordered = models.BooleanField(default=False)
+    ordered_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
